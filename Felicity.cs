32170c509b8cb83e054b38525a96c99cab781e96
Festivize <77230051+Festivize@users.noreply.github.com>
2023-12-28 03:04:10 +0800

    funny

--- Felicity.cs
+++ Felicity.cs
@@ -1,20 +1,25 @@
 ﻿using Discord;
 using Discord.WebSocket;
 using System.Threading.Channels;
-using felicity.Services;
+using Services;
+using Discord.Audio;
 
 internal class Felicity {
     public static DiscordSocketClient _client;
 
-    public static readonly string token = File.ReadAllText("C:\\Users\\fes\\dev\\repositories\\felicity\\token.txt");
+    public static readonly string token = File.ReadAllText("token.txt");
     public const ulong userID = 1059326067791376424ul;
     public const ulong generalChan = 1063456558643695656ul;
-    //public const ulong renderChan = 895185772188147722ul;
-    public const ulong renderChan = 1162759175516983389ul;
+    public const ulong renderChan = 895185772188147722ul;
+    public const ulong voiceChan = 1146857385936298125ul;
+    //public const ulong renderChan = 1162759175516983389ul;
 
-    private Task ReadyAsync() {
+    private async Task ReadyAsync() {
         Console.WriteLine($"{0} is connected!", _client.CurrentUser);
-        return Task.CompletedTask;
+        IAudioClient audioClient;
+        IVoiceChannel joinThis = _client.GetChannel(voiceChan) as IVoiceChannel;
+        if(joinThis != null) audioClient = await joinThis.ConnectAsync();
+        return;
     }
 
     private Task LogAsync(LogMessage msg) {
@@ -23,7 +28,10 @@ internal class Felicity {
     }
 
     public async Task MainAsync() {
-        _client = new DiscordSocketClient();
+        _client = new DiscordSocketClient(new DiscordSocketConfig {
+            MessageCacheSize = 100,
+            GatewayIntents = GatewayIntents.All
+        });
 
         _client.Ready += ReadyAsync;
         _client.Log += LogAsync;
--- Services/Messaging.cs
+++ Services/Messaging.cs
@@ -6,21 +6,21 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace felicity.Services {
+namespace Services {
     internal class Messaging {
-        public static Task receiveMessage(SocketMessage message) {
+        public static async Task receiveMessage(SocketMessage message) {
             IMessageChannel renderChan = Felicity._client.GetChannel(Felicity.renderChan) as IMessageChannel;
 
-            if(message.Channel.Id != renderChan.Id || message.Author.IsBot) return Task.CompletedTask;
-            if(message.Attachments.Any()) {
-                foreach(Attachment attachment in message.Attachments) Console.WriteLine(attachment.Filename);
-                Console.WriteLine("fuck you");
-                return Task.CompletedTask;
-            }
+            if(message.Channel.Id != renderChan.Id || message.Author.IsBot) return;
+            if(message.Attachments.Any() || message.Embeds.Any()) return;
 
-            message.Channel.SendMessageAsync(":joy:");
+            // message violates the festive rule
+            await message.DeleteAsync();
+            IUserMessage deleteThis = await renderChan.SendMessageAsync("Please do not chat in this channel!");
+            await Task.Delay(3500);
+            await deleteThis.DeleteAsync();
 
-            return Task.CompletedTask;
+            return;
         }
     }
 }
